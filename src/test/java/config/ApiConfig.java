1503841a2bc87134c0e72cb6947c24633ffb87af
vika.bagratunyan <bagratunaviktoria@gmail.com>
2026-04-25 18:47:19 +0300

    feat(api_test): added new feature

--- src/test/java/config/ApiConfig.java
+++ src/test/java/config/ApiConfig.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.BeforeAll;
 
 public class ApiConfig {
 
-    protected static final String BASE_URL = "http://10.82.196.214:8085";
+    protected static final String BASE_URL = "http://localhost:8085";
     protected static final String API_KEY = "bookstore-2026-secret";
 
     @BeforeAll
